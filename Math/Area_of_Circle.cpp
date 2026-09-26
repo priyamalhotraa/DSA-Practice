6eaa533d1268886c80714ee85d6b4a73b61f6840
Priya Malhotra <priyamalhotra14012005@gmail.com>
2026-09-26 23:48:14 +0530

    Create Area_of_Circle.cpp

---
+++ Math/Area_of_Circle.cpp
@@ -0,0 +1,21 @@
+```cpp
+// Problem: Area of Circle
+// Topic: Math
+
+/* CH
+APPROACH:
+Find the area of a circle using its radius.
+
+Use the formula:
+    Area = π × r × r
+
+Return the calculated area.
+*/
+
+class Solution {
+public:
+    double areaOfCircle(int r) {
+        return M_PI * r * r;
+    }
+};
+```
